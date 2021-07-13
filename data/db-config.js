4bb739085546ca25ad4d386f69b6556b958a7510
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-13 18:58:51 +0200

    wip

--- data/db-config.js
+++ data/db-config.js
@@ -1,3 +1,7 @@
+const knex = require('knex')
+
 const configurations = require('../knexfile')
 
 const environment = process.env.NODE_ENV || 'development'
+
+module.export = knex(configurations[environment])
