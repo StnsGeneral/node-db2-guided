1b5b0ea24aa8147b67429a48d5b366761be832d0
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-13 19:39:35 +0200

    wip

--- data/migrations/20210713171117_create-fruits-table.js
+++ data/migrations/20210713171117_create-fruits-table.js
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
   return knex.schema.createTable('fruits', table => {
-    table.increments('fruit_id') // shortcut for creating an 'id' column that is the primary key
+    table.increments('fruit_id') // shortcut for creating a primary key
     table.text('fruit_name', 128) // column of data type 'text' named 'fruit_name'
   })
 };
