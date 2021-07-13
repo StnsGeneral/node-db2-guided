e2f2f6fe78c3ce9d7962072002ce8dbf520906e4
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-13 19:03:03 +0200

    wip

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = { // which one is used depends on value of an env variable (NOD
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/produce.db3', // with others it'd be a URL
+      filename: './data/produce.db3', // with other SQLs it'd be a URL
     },
     useNullAsDefault: true, // only required in sqlite
     migrations: {
