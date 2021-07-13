94c117b095846fa7424eeda0c60010db3c185ee3
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-13 18:52:30 +0200

    wip

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,12 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/produce.db3',
+      filename: './data/produce.db3', // with others it'd be a URL
     },
-    useNullAsDefault: true,
+    useNullAsDefault: true, // only required in sqlite
+    migrations: {
+      directory: './data/migrations',
+    }
 
   },
   production: {},
