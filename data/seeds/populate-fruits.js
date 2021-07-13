48554fda501255c0f19739019a8517779b7624bf
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-13 20:07:03 +0200

    wip

--- data/seeds/populate-fruits.js
+++ data/seeds/populate-fruits.js
@@ -5,7 +5,7 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex('fruits').insert([
         // DO NOT ADD ids
-        { fruit_name: 'pear', avg_weight_oz: 1.5, color: 'green', delicious: false },
+        { fruit_name: 'pear', avg_weight_oz: 1.5, color: 'green', delicious: true },
         { fruit_name: 'apple', avg_weight_oz: 2.5, color: 'red' },
         { fruit_name: 'pear', avg_weight_oz: 5 }
       ]);
