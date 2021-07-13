892d3139484067977ca11b483196b54293b4d3c8
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-13 19:49:51 +0200

    wip

--- data/migrations/20210713171117_create-fruits-table.js
+++ data/migrations/20210713171117_create-fruits-table.js
@@ -3,7 +3,10 @@ exports.up = function (knex) {
     .createTable('fruits', table => {
       table.increments('fruit_id') // shortcut for creating a primary key
       table.text('fruit_name', 128) // column of data type 'text' named 'fruit_name'
+        .unique()
+        .notNullable()
       table.decimal('avg_weight_oz') // column of data type 'decimal' named 'avg_weight_oz'
+        .notNullable()
       table.boolean('delicious')
     })
     .createTable('users', table => {
