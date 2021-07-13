e13e4ace205351b7f5f0baa71bf6d3d566d8847d
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-13 19:49:02 +0200

    wip

--- data/migrations/20210713171117_create-fruits-table.js
+++ data/migrations/20210713171117_create-fruits-table.js
@@ -1,15 +1,16 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('fruits', table => {
-    table.increments('fruit_id') // shortcut for creating a primary key
-    table.text('fruit_name', 128) // column of data type 'text' named 'fruit_name'
-    table.decimal('avg_weight_oz') // column of data type 'decimal' named 'avg_weight_oz'
-    table.boolean('delicious')
-  })
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('fruits', table => {
+      table.increments('fruit_id') // shortcut for creating a primary key
+      table.text('fruit_name', 128) // column of data type 'text' named 'fruit_name'
+      table.decimal('avg_weight_oz') // column of data type 'decimal' named 'avg_weight_oz'
+      table.boolean('delicious')
+    })
     .createTable('users', table => {
       table.increments('user_id')
     })
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTableIfExists('fruits');
 };
